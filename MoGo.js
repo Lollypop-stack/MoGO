fb72b53e014a50643e5eb3e34965111459f10f34
Lollypop-stack <77138271+Lollypop-stack@users.noreply.github.com>
2022-08-27 11:14:44 +0600

    Add files via upload

---
+++ MoGo.js
@@ -0,0 +1,34 @@
+function OpenPHOTODescript() {
+    document.getElementById('PHOTOdescript').style.display = 'flex';
+    document.getElementById('UArrow').style.display = 'flex';
+    document.getElementById('DArrow').style.display = 'none';
+   }
+   function closePHOTOdescript() {
+    document.getElementById('PHOTOdescript').style.display = 'none';
+    document.getElementById('UArrow').style.display = 'none';
+    document.getElementById('DArrow').style.display = 'flex';
+   }
+
+
+   function OpenCREATEDescript() {
+    document.getElementById('CREATEUArrow').style.display = 'flex';
+    document.getElementById('CREATEDArrow').style.display = 'none';
+    document.getElementById('CREATIVEdescript').style.display = 'flex';
+   }
+   function closeCREATEdescript() {
+    document.getElementById('CREATIVEdescript').style.display = 'none';
+    document.getElementById('CREATEUArrow').style.display = 'none';
+    document.getElementById('CREATEDArrow').style.display = 'flex';
+   }
+
+
+   function OpenWDDescript() {
+    document.getElementById('WDdescript').style.display = 'flex';
+    document.getElementById('WDUArrow').style.display = 'flex';
+    document.getElementById('WDDArrow').style.display = 'none';
+   }
+   function closeWDdescript() {
+    document.getElementById('WDdescript').style.display = 'none';
+    document.getElementById('WDUArrow').style.display = 'none';
+    document.getElementById('WDDArrow').style.display = 'flex';
+   }
